7a1c2a5668c7ca84f4344388b4f2283394c9ae61
Morad <Morad-96@hotmail.com>
2023-01-26 10:59:53 +0100

    fourth commit

--- EnviarFicheroUDP/src/enviarFichero/Cliente.java
+++ EnviarFicheroUDP/src/enviarFichero/Cliente.java
@@ -1,7 +1,6 @@
 
 package enviarFichero;
 
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,43 +9,49 @@ import java.util.Scanner;
 
 public class Cliente {
 	public static void main(String[] args) throws IOException {
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		final String HOST = "localhost";
 		int puerto_Servidor = 6000;
 
-		// INICIAMOS SERVIDOR CLIENTE
-		System.out.println("Cliente conectado...");
-		InetAddress direcion_servidor = InetAddress.getByName(HOST);
-		DatagramSocket socketCliente = new DatagramSocket();
+		while (true) {
 
-		System.out.println("Introduce el nombre del fichero");
-		String nombreArchivo = sc.nextLine();
+			// INICIAMOS SERVIDOR CLIENTE
+			System.out.println("Cliente conectado...");
+			InetAddress direcion_servidor = InetAddress.getByName(HOST);
+			DatagramSocket socketCliente = new DatagramSocket();
 
-		// ENVIAMOS EL NOMBRE DEL FICHERO AL SERVIDOR
-		byte[] enviar = String.valueOf(nombreArchivo).getBytes();
-		DatagramPacket paqueteEnviar = new DatagramPacket(enviar, enviar.length, direcion_servidor, puerto_Servidor);
-		socketCliente.send(paqueteEnviar);
+			System.out.println("Introduce el nombre del fichero");
+			String nombreArchivo = sc.nextLine();
 
-		// recibir respuesta servidor, es decir el contenido del archivo
-		byte[] recibido = new byte[1024];
-		DatagramPacket paqueteRecibido = new DatagramPacket(recibido, recibido.length);
-		socketCliente.receive(paqueteRecibido);
+			// ENVIAMOS EL NOMBRE DEL FICHERO AL SERVIDOR
+			byte[] enviar = String.valueOf(nombreArchivo).getBytes();
+			DatagramPacket paqueteEnviar = new DatagramPacket(enviar, enviar.length, direcion_servidor,
+					puerto_Servidor);
+			socketCliente.send(paqueteEnviar);
 
-		String contenido = new String(paqueteRecibido.getData());
+			// recibir respuesta servidor, es decir el contenido del archivo
+			byte[] recibido = new byte[1024];
+			DatagramPacket paqueteRecibido = new DatagramPacket(recibido, recibido.length);
+			socketCliente.receive(paqueteRecibido);
 
-		if (contenido.equals("404: File not found")) {
-			System.out.println(contenido);
+			String contenido = new String(paqueteRecibido.getData()).trim();
 
-		} else {
+			if (contenido.equals("404: File not found")) {
+				System.out.println(contenido);
+				System.out.println("Escribe bien el nombre del fichero o ruta!");
 
-			System.out.println("Contenido del fichero");
-			System.out.println(contenido);
+			} else {
+
+				System.out.println("Contenido del fichero");
+				System.out.println(contenido);
+				socketCliente.close();
+				break;
+
+			}
 
 		}
-
-		socketCliente.close();
 
 	}
 
--- EnviarFicheroUDP/src/enviarFichero/Servidor.java
+++ EnviarFicheroUDP/src/enviarFichero/Servidor.java
@@ -9,112 +9,88 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-
 public class Servidor {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		try {
-			
+
 			int puertoSocket = 6000;
-			
-			
-			//iniciamos el servidor 
+
+			// iniciamos el servidor
 			DatagramSocket socketServidor = new DatagramSocket(puertoSocket);
 			byte[] recibido = new byte[1024];
-			
-			
 
-		//	while (true) {
-				
+			while (true) {
+
 				System.out.println("Servidor escuchando... esperando al cliente ");
-				DatagramPacket paqueteRecibido = new DatagramPacket(recibido, recibido.length);         	
-				//recibimos el paquete del cliente
+				DatagramPacket paqueteRecibido = new DatagramPacket(recibido, recibido.length);
+				// recibimos el paquete del cliente
 				socketServidor.receive(paqueteRecibido);
-				
-				//obtenemos informacion del cliente
+
+				// obtenemos informacion del cliente
 				InetAddress direccion = paqueteRecibido.getAddress();
 				int puerto = paqueteRecibido.getPort();
-				
+
 				String nombreArchivo = new String(paqueteRecibido.getData()).trim();
-			    System.out.println("cliente ha pedido leer el archivo: " + nombreArchivo );
-			    
-			    
-			    
-			    //leemos
-			    String contenidoFichero;
-			    contenidoFichero = leerFichero(nombreArchivo);
-			    
-			    
-			    if(contenidoFichero!=null) {
-			    	 byte [] enviar = contenidoFichero.getBytes();
-			    	 DatagramPacket respuesta = new DatagramPacket(enviar,  enviar.length, direccion, puerto);
-					 socketServidor.send(respuesta);
-					
-					 
-			    	
-			    }else {
-			    	contenidoFichero = "404: File not found";
-			    	 byte [] enviar = contenidoFichero.getBytes();
-			    	 DatagramPacket respuesta = new DatagramPacket(enviar,  enviar.length, direccion, puerto);
-					 socketServidor.send(respuesta);
-					
-			    }
-			   
-			    
-			 
-				 socketServidor.close();
-			//}
-			
-			
-			
-			
-			
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+				System.out.println("cliente ha pedido leer el archivo: " + nombreArchivo);
+
+				// leemos
+				String contenidoFichero;
+				contenidoFichero = leerFichero(nombreArchivo);
+
+				if (contenidoFichero.equals("404: File not found")) {
+					byte[] enviar = contenidoFichero.getBytes();
+					DatagramPacket respuesta = new DatagramPacket(enviar, enviar.length, direccion, puerto);
+					socketServidor.send(respuesta);
+
+				} else {
+					byte[] enviar = contenidoFichero.getBytes();
+					DatagramPacket respuesta = new DatagramPacket(enviar, enviar.length, direccion, puerto);
+					socketServidor.send(respuesta);
+					socketServidor.close();
+					break;
+				}
+
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-		
-		
 	
 	
-		
-	
-	}
 
 	public static String leerFichero(String nombre) {
-		
+
 		StringBuilder sb = new StringBuilder();
 		try {
-		
-		
+
 			File fichero = new File(nombre);
-			
+
 			FileReader fr = new FileReader(fichero);
 			BufferedReader br = new BufferedReader(fr);
-			
-			
+
 			String linea = br.readLine();
-			
-			while(linea!=null) {
-				
+
+			while (linea != null) {
+
 				sb.append(linea);
-				//separamos por lineas el texto
+				// separamos por lineas el texto
 				sb.append(System.lineSeparator());
 				linea = br.readLine();
 			}
-			
+
 			br.close();
 			fr.close();
-		
-		
-			
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			sb.append("404: File not found");
-			
+
 		}
-		
+
 		return sb.toString();
 	}
 
